78fe913de3d2de50d1cf936ef93a065807e99eb3
Drago1234 <dong.760@osu.edu>
2020-03-24 12:01:12 -0400

    added perceptron_class.py

--- fabeng4999/data/perceptron_demo.py
+++ fabeng4999/data/perceptron_demo.py
@@ -1,23 +1,29 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-
-def sigmoid_derivative(x):
-    return x*(1-x)
-
 training_inputs = np.array([[0,0,1],
                             [0,1,1],
                             [1,0,1],
                             [1,1,1]])
 training_output = np.array([[0,1,1,1]]).T
-print(np.shape(training_output))
+# print(np.shape(training_output))
 
 # initialize the weight within range(-1,1), there are many other science technique for this, e.g. initialize to zero.
 synaptic_weights = 2 * np.random.random((3,1))-1
 
 print("Random starting synpatic weigths: ")
 print(synaptic_weights)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x*(1-x)
+
+def predict(inputs):
+    inputs = inputs.astype(float)
+    output = sigmoid(np.dot(inputs, synaptic_weights))
+    return output
+
 
 for iteration in range(20000):
     input_layer = training_inputs
@@ -32,3 +38,5 @@ print("Synaptic weight after training: ")
 print(synaptic_weights)
 print('Output after training: ')
 print(output_layer)
+
+
